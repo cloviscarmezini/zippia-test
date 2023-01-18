a38115f942330057e5c910dbb8d79687b664b2e6
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-01-18 20:43:42 -0300

    added link to jobs page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen w-screen flex justify-center items-center flex-col">
       <h1>Zippia test</h1>
+      <Link
+        href="/test/jobs"
+        className="flex items-center mt-4 bg-gray-700  text-white uppercase p-4 rounded font-bold text-sm"
+      >
+        Go to jobs list
+      </Link>
     </div>
   )
 }
